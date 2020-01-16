b256d3da2de85877f47f2274e7e5982d7dda71e2
wuhaiyun <420133463@qq.com>
2020-01-16 16:21:14 +0800

    添加登录按钮检查

--- src/app/logon/logon.component.ts
+++ src/app/logon/logon.component.ts
@@ -14,16 +14,17 @@ export class LogonComponent implements OnInit {
 
   submitForm(): void{
     for(const i in this.validateForm.controls) {
+      // 手动更改表单状态时，例如 markAsDirty 后，需要执行 updateValueAndValidity 通知 nz-form-control 进行状态变更。
+      // 标记表单控件值已改变
       this.validateForm.controls[i].markAsDirty();
+      // 重新计算表单控件的值和验证状态
       this.validateForm.controls[i].updateValueAndValidity();
     }
     this.logonService.login().subscribe(
       response => {
         this.response = response;
         console.log(this.response);
-      }
-    )
-    console.log(this.response);
+      });
   }
 
   constructor(
@@ -35,7 +36,7 @@ export class LogonComponent implements OnInit {
       userName: [null, [Validators.required]],
       password: [null, [Validators.required]],
       remember: [true]
-    })
+    });
   }
 
 }
