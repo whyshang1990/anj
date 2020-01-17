75a06e382f625abc8df2185f7aa4b030f7c74f53
wuhaiyun <420133463@qq.com>
2020-01-17 19:24:02 +0800

    主页应用子路由

--- src/app/index/index-routing.module.ts
+++ src/app/index/index-routing.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppIndexComponent } from './app-index/app-index.component';
+import { HomeComponent } from './home/home.component';
+import { AccountBookComponent } from './account-book/account-book.component';
 
 
-const routes: Routes = [
-  { path: 'index', component: AppIndexComponent },
+const indexRoutes: Routes = [
+  { 
+    path: 'index', 
+    component: AppIndexComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'account-book', component: AccountBookComponent }
+    ]
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(indexRoutes)],
   exports: [RouterModule]
 })
 export class IndexRoutingModule { }
