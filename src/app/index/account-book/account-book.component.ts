98b1f9e930ac343deb7e7891a73c368ae0b14a18
wuhaiyun <420133463@qq.com>
2020-01-19 12:46:10 +0800

    修改页面布局

--- src/app/index/account-book/account-book.component.ts
+++ src/app/index/account-book/account-book.component.ts
@@ -1,4 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NzCascaderOption } from 'ng-zorro-antd';
+
+const options = [
+  {
+    value: 'zhejiang',
+    label: 'Zhejiang',
+    children: [
+      {
+        value: 'hangzhou',
+        label: 'Hangzhou',
+        children: [
+          {
+            value: 'xihu',
+            label: 'West Lake',
+            isLeaf: true
+          }
+        ]
+      },
+      {
+        value: 'ningbo',
+        label: 'Ningbo',
+        isLeaf: true
+      }
+    ]
+  },
+  {
+    value: 'jiangsu',
+    label: 'Jiangsu',
+    children: [
+      {
+        value: 'nanjing',
+        label: 'Nanjing',
+        children: [
+          {
+            value: 'zhonghuamen',
+            label: 'Zhong Hua Men',
+            isLeaf: true
+          }
+        ]
+      }
+    ]
+  }
+];
 
 @Component({
   selector: 'app-account-book',
@@ -6,10 +50,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-book.component.css']
 })
 export class AccountBookComponent implements OnInit {
+ // nz-drawer是否可见
+ visible = false;
+ reactiveForm: FormGroup;
+ nzOptions: NzCascaderOption[] = options;
+ values: string[] | null = null;
 
-  constructor() { }
+ constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit() {
-  }
+ ngOnInit() {
+   this.reactiveForm = this.formBuilder.group({
+     amount: [null, [Validators.required]],
+     category: [null, [Validators.required]],
+     subCategory: [null, [Validators.required]],
+     remarks: [null]
+   })
+ }
 
+ submitForm(): void {
+
+ }
+
+ private createTrans(): void {
+   this.visible = true;
+ }
+ private closeTrans(): void {
+   this.visible = false;
+ }
+
+ private onChanges(values: string[]): void {
+   console.log(values, this.values);
+ }
 }
--- src/app/index/home/home.component.ts
+++ src/app/index/home/home.component.ts
@@ -1,48 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { NzCascaderOption } from 'ng-zorro-antd/cascader';
 
-const options = [
-  {
-    value: 'zhejiang',
-    label: 'Zhejiang',
-    children: [
-      {
-        value: 'hangzhou',
-        label: 'Hangzhou',
-        children: [
-          {
-            value: 'xihu',
-            label: 'West Lake',
-            isLeaf: true
-          }
-        ]
-      },
-      {
-        value: 'ningbo',
-        label: 'Ningbo',
-        isLeaf: true
-      }
-    ]
-  },
-  {
-    value: 'jiangsu',
-    label: 'Jiangsu',
-    children: [
-      {
-        value: 'nanjing',
-        label: 'Nanjing',
-        children: [
-          {
-            value: 'zhonghuamen',
-            label: 'Zhong Hua Men',
-            isLeaf: true
-          }
-        ]
-      }
-    ]
-  }
-];
 
 @Component({
   selector: 'app-home',
@@ -50,35 +7,12 @@ const options = [
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  // nz-drawer是否可见
-  visible = false;
-  reactiveForm: FormGroup;
-  nzOptions: NzCascaderOption[] = options;
-  values: string[] | null = null;
 
-  constructor(private formBuilder: FormBuilder) { }
+
+  constructor() { }
 
   ngOnInit() {
-    this.reactiveForm = this.formBuilder.group({
-      amount: [null, [Validators.required]],
-      category: [null, [Validators.required]],
-      subCategory: [null, [Validators.required]],
-      remarks: [null]
-    })
+   
   }
 
-  submitForm(): void {
-
-  }
-
-  private createTrans(): void {
-    this.visible = true;
-  }
-  private closeTrans(): void {
-    this.visible = false;
-  }
-
-  private onChanges(values: string[]): void {
-    console.log(values, this.values);
-  }
 }
